830b901e1c24fe28651ee90bdfffa926377f4fc4
Minhaz Chowdhury <63835906+Minhaz2019336040@users.noreply.github.com>
2021-01-25 09:33:56 +0600

    primary upload

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\githubpoint\reactdjango\real state application\realest_estate\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
